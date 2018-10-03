2a94f7b02c843a8fdf805f5e985590ccc8697556
Fernando Ruaro <landell.ruaro@gmail.com>
2018-10-02 23:03:34 -0300

    Adding s3

---
+++ lambda/s3/main.tf
@@ -0,0 +1,42 @@
+locals {
+  s3_policy = [<<EOF
+{
+      "Effect": "Allow",
+      "Action": ["s3:GetObject"],
+      "Resource": "${var.bucket_arn}"
+}
+EOF
+  ]
+}
+
+resource "aws_lambda_permission" "lambda" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = "${module.default.lambda_function_name}"
+  principal     = "s3.amazonaws.com"
+  source_arn    = "${var.bucket_arn}"
+}
+
+resource "aws_s3_bucket_notification" "lambda" {
+  bucket = "${var.bucket_name}"
+
+  lambda_function {
+    lambda_function_arn = "${module.default.lambda_arn}"
+    events              = ["s3:ObjectCreated:*", "s3:ObjectRemoved:*"]
+    filter_suffix       = "${var.bucket_notification_extension}"
+  }
+}
+
+module "default" {
+  source = "../default"
+
+  path                    = "${var.path}"
+  timeout                 = "${var.timeout}"
+  variables               = "${var.variables}"
+  handler                 = "${var.handler}"
+  function_name           = "${var.function_name}"
+  vpc_config              = "${var.vpc_config}"
+  extra_policy_statements = "${compact(concat(local.kinesis_policy,var.extra_policy_statements))}"
+  vpc_config_enabled      = "${var.vpc_config_enabled}"
+  runtime                 = "${var.runtime}"
+}
---
+++ lambda/s3/variables.tf
@@ -0,0 +1,53 @@
+variable "kinesis_stream_arn" {
+  description = "The stream arn to be processed"
+}
+
+variable "batch_size" {
+  default = 200
+}
+
+variable "function_name" {
+  description = "The name of the function"
+}
+
+variable "path" {
+  description = "The function's folder (the folder will be sent as the code for the lambda)"
+}
+
+variable "handler" {
+  description = "The handler of the function (file.method)"
+}
+
+variable "variables" {
+  type = "map"
+
+  default = {
+    EMPTY_ENVIRONMENT = true
+  }
+}
+
+variable "timeout" {
+  default = 3
+}
+
+variable "vpc_config_enabled" {
+  default = false
+}
+
+variable "vpc_config" {
+  default = {}
+}
+
+variable "extra_policy_statements" {
+  type    = "list"
+  default = []
+}
+
+variable "runtime" {
+  default = "nodejs8.10"
+}
+
+variable "bucket_name" {}
+variable "bucket_arn" {}
+
+variable "bucket_notification_extension" {}
