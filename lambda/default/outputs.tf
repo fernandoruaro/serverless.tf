52c12dd75fb9df135913f98c1b0296e1294bd59d
Fernando Ruaro <landell.ruaro@gmail.com>
2021-03-07 13:59:15 -0300

    Fix empty tuples

--- lambda/default/outputs.tf
+++ lambda/default/outputs.tf
@@ -1,11 +1,11 @@
 output "lambda_arn" {
-  value = aws_lambda_function.lambda.arn
+  value = aws_lambda_function.lambda.arn ? aws_lambda_function.lambda.arn : null
 }
 
 output "lambda_invoke_arn" {
-  value = aws_lambda_function.lambda.invoke_arn
+  value = aws_lambda_function.lambda.invoke_arn ? aws_lambda_function.lambda.invoke_arn : null
 }
 
 output "lambda_function_name" {
-  value = aws_lambda_function.lambda.function_name
+  value = aws_lambda_function.lambda.function_name ? aws_lambda_function.lambda.function_name : null
 }
