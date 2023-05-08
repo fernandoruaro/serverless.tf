99613078a26aa13107368f1d05b0cfb61be0ebff
kpedrok <43593052+kpedrok@users.noreply.github.com>
2023-05-08 11:42:00 -0300

    chore: add tags to lambda dynamodb

--- lambda/dynamodb/main.tf
+++ lambda/dynamodb/main.tf
@@ -14,7 +14,7 @@ locals {
 {
     "Effect": "Allow",
     "Action": ["dynamodb:*"],
-    "Resource": "arn:aws:dynamodb:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:table/${element(split("/", var.dynamodb_stream_arn),1)}/stream/*"
+    "Resource": "arn:aws:dynamodb:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:table/${element(split("/", var.dynamodb_stream_arn), 1)}/stream/*"
 }
 EOF
   ]
@@ -31,11 +31,13 @@ module "default" {
   handler                        = var.handler
   function_name                  = var.function_name
   vpc_config                     = var.vpc_config
-  extra_policy_statements        = compact(concat(local.dynamodb_policy,var.extra_policy_statements))
+  extra_policy_statements        = compact(concat(local.dynamodb_policy, var.extra_policy_statements))
   vpc_config_enabled             = var.vpc_config_enabled
   runtime                        = var.runtime
   memory_size                    = var.memory_size
   reserved_concurrent_executions = var.reserved_concurrent_executions
   layers                         = var.layers
   source_code_hash               = var.source_code_hash
+  tags                           = var.tags
+
 }
